83abb07f8d5b7d0473fc8c18f99139d93604a53b
DarioKaczmarowski <dario.kaczmarowski@student.vives.be>
2020-02-05 12:31:12 +0100

    update

--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,7 @@
-module $MODULE_NAME$ {
+module Project {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.desktop;
+
+    opens sample ;
 }
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,13 @@
 package sample;
 
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+
 public class Controller {
+
+    public TextField txtGetal1;
+    public TextField txtGetal2;
+    public Label lblResult;
+
+
 }
